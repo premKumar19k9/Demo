304d7065310ff15c3ae372b3d368b964c919f064
Prem Kumar T <billaprem84@gmail.com>
2025-08-01 01:37:53 +0530

    Initial commit

---
+++ demo/src/main/java/com/domo/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.domo.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		log.info("Starting Application");
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		log.info("Started Application {}", applicationContext.getEnvironment().getProperty("spring.application.name"));
+	}
+
+}
---
+++ demo/src/test/java/com/domo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
