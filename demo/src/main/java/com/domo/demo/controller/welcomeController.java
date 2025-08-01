8386d441dec7cc2eed6bd4c62857efa3fa419e13
Prem Kumar T <billaprem84@gmail.com>
2025-08-01 20:09:25 +0530

    Docker practices | Local

---
+++ demo/src/main/java/com/domo/demo/controller/welcomeController.java
@@ -0,0 +1,18 @@
+package com.domo.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/welcome")
+public class welcomeController {
+
+    @GetMapping
+    public String greet() {
+        log.info("Accessed the welcome endpoint");
+        return "<html> <b><i>Welcome to the Domo Application! <b><i> </html>";
+    }
+}
